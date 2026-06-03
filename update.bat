0e6dd5403d1e1ce6cf20ccf4442f81005bf7d4b8
chrishor29 <chrishor29@gmail.com>
2026-06-03 11:24:24 +0200

    update

--- update.bat
+++ update.bat
@@ -1,13 +1,9 @@
 
-REM git rm videos\drainage.mp4
-
 git config --global user.name "chrishor29"
 git config --global user.email "chrishor29@gmail.com"
 
-git remote add origin
-git pull origin master
 git add .
-git commit -m "make some changes"
-git push origin master
+git commit -m "update"
 
+git push origin main
 pause
